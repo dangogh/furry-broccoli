e57973b2815dffa157694a5d86e16331ec9dda7f
Dan Kirkwood <dangogh@gmail.com>
2021-04-16 11:24:28 -0600

    Call routes endpoint

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
 
 const MTBA_API = "https://api-v3.mbta.com"
 
-func get(path string, attrs map[string]string) {
-	http.Get(MTBA_API + "/" + path)
-}
 func main() {
+	req, err := http.NewRequest("GET", MTBA_API+"/routes", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	q := req.URL.Query()
+
+	// TODO: consts for route type to create csv string
+	q.Add("filter[type]", "0,1")
+
+	client := http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dec := json.NewDecoder(resp.Body)
+
+	var m map[string]interface{}
+
+	err = dec.Decode(&m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent(" ", "  ")
+
+	err = enc.Encode(m)
 }
