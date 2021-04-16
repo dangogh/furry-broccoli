41c394664e402ff7bcaf82bccfcad7176aa4b6b1
Dan Kirkwood <dangogh@gmail.com>
2021-04-16 12:15:51 -0600

    Call routes endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,24 +10,32 @@ import (
 
 const MTBA_API = "https://api-v3.mbta.com"
 
-func main() {
-	req, err := http.NewRequest("GET", MTBA_API+"/routes", nil)
+func routesRequest(base string) (*http.Request, error) {
+	req, err := http.NewRequest("GET", base+"/routes", nil)
 	if err != nil {
-		log.Fatal(err)
+		return req, err
 	}
 
 	q := req.URL.Query()
 
-	// TODO: consts for route type to create csv string
+	// TODO: add consts for vehicle type to for csv string
+	// only include light rail (0) and heavy rail (1)
 	q.Add("filter[type]", "0,1")
 
+	q.Add("fields[route]", "long_name")
+	return req, nil
+}
+
+func main() {
 	client := http.Client{}
 
-	resp, err := client.Do(req)
+	req, err := routesRequest(MTBA_API)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println(req.URL.RawQuery)
+	resp, err := client.Do(req)
 	dec := json.NewDecoder(resp.Body)
 
 	var m map[string]interface{}
