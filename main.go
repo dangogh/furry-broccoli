f4a2cb172ec56f9161760a88b2f8b467a085a448
Dan Kirkwood <dangogh@gmail.com>
2021-04-16 18:12:27 -0600

    Choosing a line

--- main.go
+++ main.go
@@ -2,51 +2,121 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
+	"strconv"
+
+	"github.com/manifoldco/promptui"
 )
 
 const MTBA_API = "https://api-v3.mbta.com"
 
-func routesRequest(base string) (*http.Request, error) {
-	req, err := http.NewRequest("GET", base+"/routes", nil)
+type Attribute struct {
+	Name string `json:"long_name"`
+}
+
+type AttrMap struct {
+	ID         string    `json:"id"`
+	Attributes Attribute `json:"attributes"`
+}
+
+type Result struct {
+	Data []AttrMap `json:"data"`
+}
+
+type client struct {
+	http.Client
+}
+
+func newRequest(base, path string, params url.Values) (*http.Request, error) {
+	uri, err := url.Parse(base)
 	if err != nil {
-		return req, err
+		return nil, err
 	}
 
-	q := req.URL.Query()
+	uri.Path += path
+	uri.RawQuery = params.Encode()
 
-	// TODO: add consts for vehicle type to for csv string
-	// only include light rail (0) and heavy rail (1)
-	q.Add("filter[type]", "0,1")
+	fmt.Printf("%++v\n", uri)
+	return http.NewRequest("GET", uri.String(), nil)
+}
 
-	q.Add("fields[route]", "long_name")
-	return req, nil
+func (c *client) getRouteNames() []string {
+	// Only need the long_name for now and
+	// filter by route type light rail or heavy rail
+	params := url.Values{}
+	params.Add("fields[route]", "long_name")
+	params.Add("filter[type]", "0,1")
+
+	req, err := newRequest(MTBA_API, "routes", params)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dec := json.NewDecoder(resp.Body)
+
+	var res Result
+
+	err = dec.Decode(&res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	routes := make([]string, 0, len(res.Data))
+
+	for _, attribs := range res.Data {
+		routes = append(routes, attribs.Attributes.Name)
+	}
+
+	return routes
 }
 
 func main() {
-	client := http.Client{}
+	client := client{}
 
-	req, err := routesRequest(MTBA_API)
-	if err != nil {
-		log.Fatal(err)
+	route := client.chooseRoute()
+
+	fmt.Println("you chose ", route)
+}
+
+func (c *client) chooseRoute() string {
+	routes := c.getRouteNames()
+
+	for i, r := range routes {
+		fmt.Printf("%5d) %s\n", i, r)
 	}
 
-	fmt.Println(req.URL.RawQuery)
-	resp, err := client.Do(req)
-	dec := json.NewDecoder(resp.Body)
-
-	var m map[string]interface{}
-
-	err = dec.Decode(&m)
-	if err != nil {
-		log.Fatal(err)
+	prompt := promptui.Prompt{
+		Label: "Choose route by number",
+		Validate: func(in string) error {
+			i, err := strconv.Atoi(in)
+			if err != nil {
+				return errors.New("not a number")
+			}
+			if i < 0 || i > len(routes)-1 {
+				return errors.New("out of range")
+			}
+			return nil
+		},
 	}
 
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent(" ", "  ")
+	var result string
+	var err error
+	for {
+		result, err = prompt.Run()
+		if err == nil {
+			break
+		}
+	}
 
-	err = enc.Encode(m)
+	i, _ := strconv.Atoi(result)
+	return routes[i]
 }
